15077c7a9d90f8d72d5c0eb1b1d059ed94cf5e02
Sao I Kuan <saoikuan@gmail.com>
2022-10-01 17:03:21 -0400

    del, update, put

--- index.js
+++ index.js
@@ -1,7 +1,135 @@
-var http = require('http');
-var server = http.createServer (function (request, response) {
-     response.writeHead (200, {"Content-Type": "text/plain"});
-     response.end("Hello World\n");
-});
-server.listen (5000, '127.0.0.1');  
-console.log("Server listening on 127.0.0.1 port 5000")
+var SERVER_NAME = 'image-api'
+var PORT = 5000;
+var HOST = '127.0.0.1';
+
+var restify = require('restify')
+  // Create the restify server
+  , server = restify.createServer({ name: SERVER_NAME})
+
+    // Create the restify server
+    , server = restify.createServer({ name: SERVER_NAME})
+
+    server.listen(PORT, HOST, function () {
+    console.log('Server %s listening at %s', server.name, server.url)
+    console.log('******Resources:*******')
+    console.log('***********************')
+    console.log(' /images')
+    console.log(' /images/imageId')  
+  })
+
+  server
+  // Allow the use of POST
+  .use(restify.fullResponse())
+
+  // Maps req.body to req.params so there is no switching between them
+  .use(restify.bodyParser())
+
+// Get all images in the system
+server.get('/images', function (req, res, next) {
+
+  // Find every entity within the given collection
+  imagesSave.find({}, function (error, images) {
+
+    // Return all of the images in the system
+    res.send(images)
+  })
+})
+
+// Get a single image by their image id
+server.get('/images/:id', function (req, res, next) {
+
+    // Find a single user by their id within save
+    imagesSave.findOne({ _id: req.params.id }, function (error, image) {
+  
+      // If there are any errors, pass them to next in the correct format
+      if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+  
+      if (image) {
+        // Send the user if no issues
+        res.send(image)
+      } else {
+        // Send 404 header if the user doesn't exist
+        res.send(404)
+      }
+    })
+  })
+
+  // Create a new image
+server.post('/images', function (req, res, next) {
+
+    // Make sure name is defined
+    if (req.params.name === undefined ) {
+      // If there are any errors, pass them to next in the correct format
+      return next(new restify.InvalidArgumentError('name must be supplied'))
+    }
+    if (req.params.url === undefined ) {
+      // If there are any errors, pass them to next in the correct format
+      return next(new restify.InvalidArgumentError('url must be supplied'))
+    }
+    if (req.params.size === undefined ) {
+        // If there are any errors, pass them to next in the correct format
+        return next(new restify.InvalidArgumentError('size must be supplied'))
+    }
+    var newImage = {
+          name: req.params.name, 
+          url: req.params.url,
+          size: req.params.size
+    }
+
+  // Create the image using the persistence engine
+  imagesSave.create( newImage, function (error, image) {
+
+    // If there are any errors, pass them to next in the correct format
+    if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+
+    // Send the image if no issues
+    res.send(201, image)
+  })
+})
+
+// Update an image by their id
+server.put('/images/:id', function (req, res, next) {
+
+    // Make sure name is defined
+    if (req.params.name === undefined ) {
+      // If there are any errors, pass them to next in the correct format
+      return next(new restify.InvalidArgumentError('name must be supplied'))
+    }
+    if (req.params.url === undefined ) {
+      // If there are any errors, pass them to next in the correct format
+      return next(new restify.InvalidArgumentError('url must be supplied'))
+    }
+    if (req.params.size === undefined ) {
+        // If there are any errors, pass them to next in the correct format
+        return next(new restify.InvalidArgumentError('size must be supplied'))
+    }
+    var newImage = {
+		_id: req.params.id,
+		name: req.params.name, 
+		url: req.params.url,
+        size: req.params.size
+	}
+  // Update the iamge with the persistence engine
+  imagesSave.update(newImage, function (error, image) {
+
+    // If there are any errors, pass them to next in the correct format
+    if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+
+    // Send a 200 OK response
+    res.send(200)
+  })
+})
+
+// Delete user with the given id
+server.del('/images/:id', function (req, res, next) {
+
+    // Delete the user with the persistence engine
+    imagesSave.delete(req.params.id, function (error, image) {
+  
+      // If there are any errors, pass them to next in the correct format
+      if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+  
+      // Send a 200 OK response
+      res.send()
+    })
+  })
