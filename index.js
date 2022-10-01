74047c869a185ce7098685a2b7eb46ca60b760ee
Sao I Kuan <saoikuan@gmail.com>
2022-10-01 19:52:26 -0400

    Update logs

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 var SERVER_NAME = 'image-api'
 var PORT = 5000;
 var HOST = '127.0.0.1';
+var ENDPOINTS = '127.0.0.1:5000/images'
 
 var restify = require('restify')
+var getNum = 0
+var postNum = 0
 
     // Get a persistence engine for the users
-  , imagesSave = require('save')('images')
+    , imagesSave = require('save')('images')
 
     // Create the restify server
     , server = restify.createServer({ name: SERVER_NAME})
 
     server.listen(PORT, HOST, function () {
     console.log('Server %s listening at %s', server.name, server.url)
-    console.log('***********************')
-    console.log('******Resources:*******')
-    console.log('***********************')
+    console.log('EndPoints: %s', ENDPOINTS)
     console.log(' /images')
-    console.log(' /images/:id')  
+    console.log(' /images/:id')
+    console.log('method: GET, POST')
+    console.log('***********************')  
   })
 
   server
@@ -26,15 +29,19 @@ var restify = require('restify')
   // Maps req.body to req.params so there is no switching between them
   .use(restify.bodyParser())
 
+  
+
 // Get all images in the system
 server.get('/images', function (req, res, next) {
-
+    
   // Find every entity within the given collection
   imagesSave.find({}, function (error, images) {
 
     // Return all of the images in the system
     res.send(images)
     console.log("images GET: sending request")
+    getNum +=1
+    console.log('Processed Request Count --> GET:',getNum, "POST:", postNum)
   })
 })
 
@@ -56,7 +63,12 @@ server.get('/images/:id', function (req, res, next) {
       }
     })
     console.log("images GET: sending request")
+    getNum +=1
+    console.log('Processed Request Count --> GET:',getNum, "POST:", postNum)
   })
+
+  
+
 
   // Create a new image
 server.post('/images', function (req, res, next) {
@@ -90,7 +102,10 @@ server.post('/images', function (req, res, next) {
     res.send(201, image)
   })
   console.log("images GET: received request")
+  postNum +=1
+  console.log('Processed Request Count --> GET:',getNum, "POST:", postNum)
 })
+
 
 // Update an image by their id
 server.put('/images/:id', function (req, res, next) {
@@ -138,3 +153,5 @@ server.del('/images/:id', function (req, res, next) {
       res.send()
     })
   })
+
+
